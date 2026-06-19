969dd2b4f9d3bbfe447d499c5786a5dde705533a
Yusa <yusarosdiana7@gmail.com>
2026-06-19 14:25:51 +0700

    fix error

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
+import axios from 'axios'
 import './style.css'
 import './assets/design-tokens.css'
 import App from './App.vue'
 import router from './router'
+
+// Bypass halaman warning ngrok untuk SEMUA request axios di seluruh app
+// (request relative lewat rewrite Vercel, maupun request absolut ke domain ngrok)
+axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true'
 
 // Buat app dulu
 const app = createApp(App)
